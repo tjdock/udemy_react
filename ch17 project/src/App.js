a6d785510d4862d007144536b604faa7e9b7a75d
XuNuo <565130824@qq.com>
2018-10-14 15:19:38 +0800

    1

---
+++ ch17 project/src/App.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import Layout from './hoc/Layout/Layout';
+import {Route, Switch} from 'react-router-dom';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
+
+class App extends Component {
+  render() {
+
+    return (
+        <div>
+          <Layout>
+            <Switch>
+              <Route path='/' exact component={BurgerBuilder}/>
+              <Route path='/checkout' component={Checkout}/>
+              <Route path='/orders' component={Orders}/>
+            </Switch>
+          </Layout>
+        </div>
+    );
+  }
+}
+
+export default App;
